10754eb25394f36d864964d0dd1e29a80547ca6d
Alan Penner <metsfan@alumni.cmu.edu>
2014-01-21 14:54:20 -0800

    remove waitfordeferred.

--- lib/coinbaser.py
+++ lib/coinbaser.py
@@ -100,7 +100,7 @@ class SimpleCoinbaser(object):
     def change(self, address):
         self.address = address
         self.is_valid = False
-        self._validate()
+        (yield self._validate())
 
 
 
