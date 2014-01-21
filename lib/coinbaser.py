da9b1f8f21ea945f96397f79d7e3b116fd52dc18
Alan Penner <metsfan@alumni.cmu.edu>
2014-01-21 14:43:15 -0800

    wait for deferred?

--- lib/coinbaser.py
+++ lib/coinbaser.py
@@ -99,8 +99,7 @@ class SimpleCoinbaser(object):
     def change(self, address):
         self.address = address
         self.is_valid = False
-        (yield self._validate())
-        defer.returnValue(self.is_valid)
+        self._validate()
 
 
 
