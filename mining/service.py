0829ff5d6c86893484921d2a3504d059f1254ec1
Alan Penner <metsfan@alumni.cmu.edu>
2014-03-02 19:33:44 -0800

    attempt at async db

--- mining/service.py
+++ mining/service.py
@@ -82,7 +82,8 @@ class MiningService(GenericService):
         settings.setup()
         log.info("Updated Config")
         return True
-        
+
+    @defer.inlineCallbacks
     def authorize(self, worker_name, worker_password):
         '''Let authorize worker on this connection.'''
         
@@ -99,7 +100,7 @@ class MiningService(GenericService):
                 session['difficulty'] = settings.POOL_TARGET
             # worker_log = (valid, invalid, is_banned, diff, is_ext_diff, timestamp)
             Interfaces.worker_manager.worker_log['authorized'][worker_name] = (0, 0, False, session['difficulty'], is_ext_diff, Interfaces.timestamper.time())            
-            return True
+            defer.returnValue(True)
         else:
             ip = self.connection_ref()._get_ip()
             log.info("Failed worker authorization: IP %s", str(ip))
@@ -107,7 +108,7 @@ class MiningService(GenericService):
                 del session['authorized'][worker_name]
             if worker_name in Interfaces.worker_manager.worker_log['authorized']:
                 del Interfaces.worker_manager.worker_log['authorized'][worker_name]
-            return False
+            defer.returnValue(False)
         
     def subscribe(self, *args):
         '''Subscribe for receiving mining jobs. This will
