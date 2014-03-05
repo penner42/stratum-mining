f8d70493786234109bf5d29d9907f29f8c417464
Alan Penner <metsfan@alumni.cmu.edu>
2014-03-05 12:01:11 -0800

    converting import_shares to executemany

--- mining/DB_Mysql_Vardiff_Multicoin.py
+++ mining/DB_Mysql_Vardiff_Multicoin.py
@@ -31,7 +31,8 @@ class DB_Mysql_Vardiff_Multicoin(DB_Mysql.DB_Mysql):
         total_shares = 0
         best_diff = 0
 
-        params = [(v[4], v[6], v[0], v[5], v[9], v[2], v[3], v[11]) for k, v in enumerate(data)]
+        # time, ip, worker_name, is_valid, invalid_reason, block_hash, difficulty, coin_name
+        params = [(v[4], v[6], v[0], 'Y' if v[5] else 'N', v[9], v[2], v[3], v[11]) for k, v in enumerate(data)]
         self.executemany("""
                 INSERT INTO `shares`
                 (time, rem_host, username, our_result,
@@ -40,35 +41,6 @@ class DB_Mysql_Vardiff_Multicoin(DB_Mysql.DB_Mysql):
                 (FROM_UNIXTIME(%s), %s, %s, %s, 'N', %s, %s, %s, %s)
                 """,
                          params)
-
-        # for k, v in enumerate(data):
-        #     # for database compatibility we are converting our_worker to Y/N format
-        #     if v[5]:
-        #         v[5] = 'Y'
-        #     else:
-        #         v[5] = 'N'
-        #
-        #     self.execute(
-        #         """
-        #         INSERT INTO `shares`
-        #         (time, rem_host, username, our_result,
-        #           upstream_result, reason, solution, difficulty, coin_name)
-        #         VALUES
-        #         (FROM_UNIXTIME(%(time)s), %(host)s,
-        #           %(uname)s,
-        #           %(lres)s, 'N', %(reason)s, %(solution)s, %(difficulty)s, %(coinname)s)
-        #         """,
-        #         {
-        #             "time": v[4],
-        #             "host": v[6],
-        #             "uname": v[0],
-        #             "lres": v[5],
-        #             "reason": v[9],
-        #             "solution": v[2],
-        #             "difficulty": v[3],
-        #             "coinname": v[11]
-        #         }
-        #     )
 
         self.dbh.commit()
     
