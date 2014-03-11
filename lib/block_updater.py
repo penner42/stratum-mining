48899519f1e68c22b58200f192ff340f75459b6e
Alan Penner <metsfan@alumni.cmu.edu>
2014-03-11 10:27:09 -0700

    use global template registry in blockupdater instead of passing in template registry and bitcoinrpc

--- lib/block_updater.py
+++ lib/block_updater.py
@@ -8,18 +8,16 @@ import lib.logger
 log = lib.logger.get_logger('block_updater')
 
 class BlockUpdater(object):
-    '''
+    """
         Polls upstream's getinfo() and detecting new block on the network.
         This will call registry.update_block when new prevhash appear.
         
         This is just failback alternative when something
         with ./litecoind -blocknotify will go wrong. 
-    '''
+    """
     
-    def __init__(self, registry, bitcoin_rpc):
+    def __init__(self):
         log.debug("Got To Block Updater")
-        self.bitcoin_rpc = bitcoin_rpc
-        self.registry = registry
         self.clock = None
         self.schedule()
                         
@@ -27,11 +25,11 @@ class BlockUpdater(object):
         when = self._get_next_time()
         log.debug("Next prevhash update in %.03f sec" % when)
         log.debug("Merkle update in next %.03f sec" % \
-                  ((self.registry.last_update + settings.MERKLE_REFRESH_INTERVAL)-Interfaces.timestamper.time()))
+                  ((Interfaces.template_registry.last_update + settings.MERKLE_REFRESH_INTERVAL)-Interfaces.timestamper.time()))
         self.clock = reactor.callLater(when, self.run)
         
     def _get_next_time(self):
-        when = settings.PREVHASH_REFRESH_INTERVAL - (Interfaces.timestamper.time() - self.registry.last_update) % \
+        when = settings.PREVHASH_REFRESH_INTERVAL - (Interfaces.timestamper.time() - Interfaces.template_registry.last_update) % \
                settings.PREVHASH_REFRESH_INTERVAL
         return when  
                      
@@ -40,23 +38,23 @@ class BlockUpdater(object):
         update = False
        
         try:             
-            if self.registry.last_block:
-                current_prevhash = "%064x" % self.registry.last_block.hashPrevBlock
+            if Interfaces.template_registry.last_block:
+                current_prevhash = "%064x" % Interfaces.template_registry.last_block.hashPrevBlock
             else:
                 current_prevhash = None
                 
             log.info("Checking for new block.")
-            prevhash = util.reverse_hash((yield self.bitcoin_rpc.prevhash()))
+            prevhash = util.reverse_hash((yield Interfaces.template_registry.bitcoin_rpc.prevhash()))
             if prevhash and prevhash != current_prevhash:
                 log.info("New block! Prevhash: %s" % prevhash)
                 update = True
             
-            elif Interfaces.timestamper.time() - self.registry.last_update >= settings.MERKLE_REFRESH_INTERVAL:
+            elif Interfaces.timestamper.time() - Interfaces.template_registry.last_update >= settings.MERKLE_REFRESH_INTERVAL:
                 log.info("Merkle update! Prevhash: %s" % prevhash)
                 update = True
                 
             if update:
-                self.registry.update_block()
+                Interfaces.template_registry.update_block()
 
         except Exception:
             log.exception("UpdateWatchdog.run failed")
