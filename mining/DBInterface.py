fccd71d6a4c27d84de1e920b9d5c2f267f9bf77a
Alan Penner <metsfan@alumni.cmu.edu>
2014-03-11 08:26:40 -0700

    remove threading on share import

--- mining/DBInterface.py
+++ mining/DBInterface.py
@@ -45,7 +45,7 @@ class DBInterface():
         return DB_Mysql.DB_Mysql()
 
     def scheduleImport(self):
-        # This scheduless the import
+        # this schedules the import
         self.queueclock = reactor.callLater(settings.DB_LOADER_CHECKTIME , self.run_import)
     
     def run_import(self):
